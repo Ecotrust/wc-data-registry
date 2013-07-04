7fa5de903aed995f15a878fde92caa887e6f7e9c
Ryan Hodges <rhodges@ecotrust.org>
2013-07-03 17:00:59 -0700

    adding marker clear button

--- static/js/map.js
+++ static/js/map.js
@@ -1,8 +1,73 @@
 var map; 
 var marker = new L.Marker([0, 0]);
+var clearControl;
+
+//Much of this code was taken from https://gist.github.com/ns-1m/2935530
+var clearBtn = L.Control.extend({
+    options: {
+        position: 'topright'
+    },
+
+    initialize: function (options) {
+        this._button = {};
+        this.setButton({
+            text: 'clear',
+            // iconUrl: '',
+            onClick: clearMarkers
+        });
+    },
+
+    onAdd: function(map) {
+        var container = L.DomUtil.create('div', 'clear-marker-button');
+        this._container = container;
+        this._update();
+        return this._container;
+    },
+
+    onRemove: function (map) {
+    },
+
+    setButton: function(options) {
+        var button = {
+            'text': options.text,
+            'iconUrl': options.iconUrl,
+            'onClick': options.onClick
+        };
+        this._button = button;
+        this._update();
+    },
+
+    _update: function () {
+        if (!this._map) {
+            return;
+        }
+         
+        this._container.innerHTML = '';
+        this._makeButton(this._button);
+    },
+         
+    _makeButton: function (button) {
+        var newButton = L.DomUtil.create('button', 'leaflet-buttons-control-button', this._container);
+        var image = L.DomUtil.create('img', 'leaflet-buttons-control-img', newButton);
+        image.setAttribute('src',button.iconUrl);
+        if(button.text !== ''){
+         
+            L.DomUtil.create('br','',newButton); //there must be a better way
+             
+            var span = L.DomUtil.create('span', 'leaflet-buttons-control-text', newButton);
+            var text = document.createTextNode(button.text); //is there an L.DomUtil for this?
+            span.appendChild(text);
+        }
+        L.DomEvent
+            .addListener(newButton, 'click', L.DomEvent.stop)
+            .addListener(newButton, 'click', button.onClick,this);
+        L.DomEvent.disableClickPropagation(newButton);
+        return newButton;
+    }
+});
+
 function init() {
-    map = L.map('map').setView([39, -120], 3);
-    //var marker = L.marker([0,0]);
+    map = L.map('map').setView([39, -127], 3);
 
     L.tileLayer('http://{s}.tile.cloudmade.com/efb495f98c9b4dac95d13787e0a72603/997/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
@@ -10,6 +75,9 @@ function init() {
     }).addTo(map);
 
     map.on('click', onMapClick);
+    clearControl = new clearBtn();
+    map.addControl(clearControl);
+    clearControl._container.hidden = true;
 }
 
 function onMapClick(e) {
@@ -21,4 +89,13 @@ function onMapClick(e) {
     app.viewModel.bbLon(e.latlng.lng);
     app.viewModel.useBb(true);
     map.addLayer(marker);
+    clearControl._container.hidden = false;
+}
+
+function clearMarkers() {
+    if (map.hasLayer(marker)){
+        map.removeLayer(marker);
+    }
+    app.viewModel.useBb(false);
+    clearControl._container.hidden = true;
 }
