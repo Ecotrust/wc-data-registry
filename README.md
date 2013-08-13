86c01e34e8a7ec3f8013350964eb0f39fca16f17
Tim Glaser <tim.glaser@ecotrust.org>
2013-08-13 16:02:57 -0700

    clear for rebuild

--- README.md
+++
@@ -1,4 +0,0 @@
-wc-data-registry
-================
-
-Data Registry for the West Coast Governors Alliance
--- static/js/local_settings_template.js
+++
@@ -1,3 +0,0 @@
-//var settings.apiKey = 'Your bing API key here.';
-var settings_solr_url = 'http://localhost:8983/solr/collection1/select?';
-var settings_gp_url = 'http://localhost:8080/geoportal';
