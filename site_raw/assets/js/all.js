6b86b7edde92297d9498b0d68cdbe1c29c5f7f11
Tim Glaser <tim.glaser@ecotrust.org>
2013-08-21 14:01:14 -0700

    Added dependencies and setup file organization

---
+++ site_raw/assets/js/all.js
@@ -0,0 +1,23 @@
+---
+---
+/**
+ * Begin dependencies scripts
+ */
+{% include bower_components/jquery/jquery.min.js %}
+{% include bower_components/angular/angular.min.js %}
+/* Begin Bootstrap */
+{% include bower_components/bootstrap/js/bootstrap-dropdown.js %}
+{% include bower_components/bootstrap/js/bootstrap-popover.js %}
+/* End Bootstrap */
+/* 
+ * End dependencies scripts
+ **/
+
+/**
+ * Begin site scripts
+ */
+
+/*
+ * End site scripts
+ **/
+  
---
+++ site_raw/assets/js/ie8.js
@@ -0,0 +1,5 @@
+---
+---
+{% include bower_components/es5-shim/es5-shim.min.js %}
+{% include bower_components/json3/lib/json3.min.js %}
+
