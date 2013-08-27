692b76001aee5fbd342edff8c72030ee01d45c32
Tim Glaser <tim.glaser@ecotrust.org>
2013-08-27 13:17:09 -0700

    CSS/HTML for search results

--- site_raw/assets/js/all.js
+++ site_raw/assets/js/all.js
@@ -6,12 +6,20 @@
 {% include bower_components/jquery/jquery.min.js %}
 {% include bower_components/angular/angular.js %}
 
+
+{% include bower_components/bootstrap/js/bootstrap-transition.js %}
+{% include bower_components/bootstrap/js/bootstrap-dropdown.js %}
+{% include bower_components/bootstrap/js/bootstrap-collapse.js %}
+{% include bower_components/bootstrap/js/bootstrap-tooltip.js %}
+/*
+ * End dependencies scripts
+ **/
+
 /**
  * Begin site scripts
  */
 {% include js/controllers/HomeCtrl.js %}
 {% include js/controllers/DiscoverCtrl.js %}
-
 /*
  * End site scripts
  **/
