c965b34ba93cfba447305bbede77ce1946bab1a1
Tim Welch <tim.j.welch@gmail.com>
2015-08-12 15:44:30 -0800

    Update all.js

--- _site_generated/assets/js/all.js
+++ _site_generated/assets/js/all.js
@@ -30982,81 +30982,185 @@ angular.module('wcodpApp').factory('packery', ['$timeout', function($timeout) {
 }]);
 angular.module('wcodpApp').factory('metadata', [function() {
 
-    // jQuery selector strings used for xml lookup of values across
-    //  various metadata formats. Note that for crossbrowser support of 
-    //  namespaced elements, we provide two selector paths: one with 
-    //  the namespaces and escaped colons; another without the namespaces.
-    var _paths = {
+    /**
+     * fieldConfigs - object with config for each metadata field we want to display
+     * Each config contains a JQuery selector(s) and a formatter for each metadata format
+     * that defines what values to extract from the metadata and how to present it ot the user
+     * Note that for crossbrowser support of namespaced elements, we provide two selector paths: one with 
+     * the namespaces (e.g. gmd) and escaped colons; another without the namespaces.
+     */ 
+    var _fieldConfigs = {
         datePublished: {
-            cd: "",
-            fgdc: "metadata > idinfo > citation > citeinfo > pubdate",
-            // fgdc: "metadata > idinfo > timeperd > timeinfo > sngdate > caldate",
-            iso: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:date > gmd\\:CI_Date > gmd\\:date > gco\\:DateTime, identificationInfo > MD_DataIdentification > citation > CI_Citation > date > CI_Date > date > DateTime",
-            //Note that for selector below, matching on 'gmd\\:' namespace within pseudoselectors 'has' and 'contains' was not returning a result, dropping this match in these places but leaving elsewhere worked for some reason.
-            isoii: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:date > gmd\\:CI_Date:has(dateType:has(CI_DateTypeCode:contains('publication'))) > gmd\\:date > gco\\:Date, identificationInfo > MD_DataIdentification > citation > CI_Citation > date > CI_Date:has(dateType:has(CI_DateTypeCode:contains('publication'))) > gdate > Date",
-            //Original iso2 selector left as secondary
-            isoii2: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:date > gmd\\:CI_Date > gmd\\:date > gco\\:Date, identificationInfo > MD_DataIdentification > citation > CI_Citation > date > CI_Date > gdate > Date"
+            cd: {
+                selectors: null,
+                formatter: "first"
+            },
+            fgdc: {
+                selectors: "metadata > idinfo > citation > citeinfo > pubdate",
+                formatter: "first"
+            },
+            iso: {
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:date > gmd\\:CI_Date > gmd\\:date > gco\\:DateTime, identificationInfo > MD_DataIdentification > citation > CI_Citation > date > CI_Date > date > DateTime",
+                formatter: "first"
+            },            
+            isoii: {
+                //Matching on 'gmd\\:' namespace within pseudoselectors 'has' and 'contains' was not returning a result, dropping this match in these places but leaving elsewhere worked for some reason.
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:date > gmd\\:CI_Date:has(dateType:has(CI_DateTypeCode:contains('publication'))) > gmd\\:date > gco\\:Date, identificationInfo > MD_DataIdentification > citation > CI_Citation > date > CI_Date:has(dateType:has(CI_DateTypeCode:contains('publication'))) > gdate > Date",
+                formatter: "first"
+            },            
+            isoii2: {
+                //Original iso2 selector left as secondary
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:date > gmd\\:CI_Date > gmd\\:date > gco\\:Date, identificationInfo > MD_DataIdentification > citation > CI_Citation > date > CI_Date > gdate > Date",
+                formatter: "first"
+            }
         },
         creator: {
-            cd: "rdf\\:RDF > rdf\\:Description > dc\\:creator",
-            fgdc: "metadata > idinfo > citation > citeinfo > origin",
-            iso: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:pointOfContact > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, identificationInfo > MD_DataIdentification > pointOfContact > CI_ResponsibleParty > organisationName > CharacterString",
-            isoii: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:citedResponsibleParty > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, identificationInfo > MD_DataIdentification > gcitation > CI_Citation > citedResponsibleParty > CI_ResponsibleParty > organisationName > CharacterString"
+            cd: {
+                selectors: "rdf\\:RDF > rdf\\:Description > dc\\:creator",
+                formatter: "first"
+            },
+            fgdc: {
+                selectors: "metadata > idinfo > citation > citeinfo > origin",
+                formatter: "list"
+            },
+            iso: {
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:pointOfContact > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, identificationInfo > MD_DataIdentification > pointOfContact > CI_ResponsibleParty > organisationName > CharacterString",
+                formatter: "first"
+            },
+            isoii: {
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:citedResponsibleParty > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, identificationInfo > MD_DataIdentification > gcitation > CI_Citation > citedResponsibleParty > CI_ResponsibleParty > organisationName > CharacterString",
+                formatter: "first"
+            }
         },
         publisher: {
-            cd: "",
-            fgdc: "metadata > distinfo > distrib > cntinfo > cntorgp > cntorg",
-            //fgdc: "metadata > idinfo > citation > citeinfo > pubinfo > publish",
-            iso: "gmd\\:contact > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, contact > CI_ResponsibleParty > organisationName > CharacterString",
-            isoii: "gmd\\:contact > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, contact > CI_ResponsibleParty > organisationName > CharacterString"
+            cd: {
+                selectors: null,
+                formatter: "first"
+            },
+            fgdc: {
+                selectors: "metadata > distinfo > distrib > cntinfo > cntorgp > cntorg",
+                formatter: "first"
+            },
+            iso: {
+                selectors: "gmd\\:contact > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, contact > CI_ResponsibleParty > organisationName > CharacterString",
+                formatter: "first"
+            },
+            isoii: {
+                selectors: "gmd\\:contact > gmd\\:CI_ResponsibleParty > gmd\\:organisationName > gco\\:CharacterString, contact > CI_ResponsibleParty > organisationName > CharacterString",
+                formatter: "first"
+            }
         },
         contactName: {
-            cd: "",
-            fgdc: "metadata > idinfo > ptcontac > cntinfo > cntorgp > cntper",
-            iso: "gmd\\:MD_Metadata > gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:pointOfContact > gmd\\:CI_ResponsibleParty > gmd\\:individualName > gco\\:CharacterString, MI_Metadata > identificationInfo > MD_DataIdentification > pointOfContact > CI_ResponsibleParty > individualName > CharacterString",
-            isoii: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:citedResponsibleParty > gmd\\:CI_ResponsibleParty > gmd\\:individualName, identificationInfo > MD_DataIdentification > citation > CI_Citation > citedResponsibleParty > CI_ResponsibleParty > individualName"
+            cd: {
+                selectors: null,
+                formatter: "first"
+            },
+            fgdc: {
+                selectors: "metadata > idinfo > ptcontac > cntinfo > cntorgp > cntper",
+                formatter: "first"
+            },
+            iso: {
+                selectors: "gmd\\:MD_Metadata > gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:pointOfContact > gmd\\:CI_ResponsibleParty > gmd\\:individualName > gco\\:CharacterString, MI_Metadata > identificationInfo > MD_DataIdentification > pointOfContact > CI_ResponsibleParty > individualName > CharacterString",
+                formatter: "first"
+            },
+            isoii: {
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:citedResponsibleParty > gmd\\:CI_ResponsibleParty > gmd\\:individualName, identificationInfo > MD_DataIdentification > citation > CI_Citation > citedResponsibleParty > CI_ResponsibleParty > individualName",
+                formatter: "first"
+            }
 		},
         contactEmail: {
-            cd: "",
-            fgdc: "metadata > idinfo > ptcontac > cntinfo > cntemail",
-            iso: "gmd\\:contactInfo > gmd\\:CI_Contact > gmd\\:address > gmd\\:CI_Address > gmd\\:electronicMailAddress > gco\\:CharacterString, contactInfo > CI_Contact > address > CI_Address > electronicMailAddress > CharacterString",
-            isoii: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:citedResponsibleParty > gmd\\:CI_ResponsibleParty > gmd\\:contactInfo > gmd\\:CI_Contact > gmd\\:address > gmd\\:CI_Address > gmd\\:electronicMailAddress > gco\\:CharacterString, identificationInfo > MD_DataIdentification > citation > CI_Citation > citedResponsibleParty > CI_ResponsibleParty > gmd\\:contactInfo > CI_Contact > address > CI_Address > electronicMailAddress > CharacterString"
+            cd: {
+                selectors: null,
+                formatter: "first"
+            },
+            fgdc: {
+                selectors: "metadata > idinfo > ptcontac > cntinfo > cntemail",
+                formatter: "first"
+            },
+            //this isoii selector purposefully put before the iso selector to prevent false positive matches on the iso selector
+            isoii: {
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:citation > gmd\\:CI_Citation > gmd\\:citedResponsibleParty > gmd\\:CI_ResponsibleParty > gmd\\:contactInfo > gmd\\:CI_Contact > gmd\\:address > gmd\\:CI_Address > gmd\\:electronicMailAddress > gco\\:CharacterString, identificationInfo > MD_DataIdentification > citation > CI_Citation > citedResponsibleParty > CI_ResponsibleParty > gmd\\:contactInfo > CI_Contact > address > CI_Address > electronicMailAddress > CharacterString",
+                formatter: "first"
+            },            
+            iso: {
+                selectors: "gmd\\:contactInfo > gmd\\:CI_Contact > gmd\\:address > gmd\\:CI_Address > gmd\\:electronicMailAddress > gco\\:CharacterString, contactInfo > CI_Contact > address > CI_Address > electronicMailAddress > CharacterString",
+                formatter: "first"
+            }                
         },
         constraints: {
-            cd: "rdf\\:RDF > rdf\\:Description > dct\\:abstract > rdf\\:value[rdf\\:resource='mxd.subject']",
-            fgdc: "metadata > idinfo > useconst",
-            iso: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:resourceConstraints > gmd\\:MD_LegalConstraints > gmd\\:otherConstraints > gco\\:CharacterString, identificationInfo > MD_DataIdentification > resourceConstraints > MD_LegalConstraints > otherConstraints > CharacterString",
-            isoii: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:resourceConstraints > gmd\\:MD_LegalConstraints > gmd\\:useLimitation > gco\\:CharacterString, identificationInfo > MD_DataIdentification > resourceConstraints > MD_LegalConstraints > useLimitation > CharacterString"
+            cd: {
+                selectors: "rdf\\:RDF > rdf\\:Description > dct\\:abstract > rdf\\:value[rdf\\:resource='mxd.subject']",
+                formatter: "first"
+            },
+            fgdc: {
+                selectors: "metadata > idinfo > useconst",
+                formatter: "first"
+            },
+            iso: {
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:resourceConstraints > gmd\\:MD_LegalConstraints > gmd\\:otherConstraints > gco\\:CharacterString, identificationInfo > MD_DataIdentification > resourceConstraints > MD_LegalConstraints > otherConstraints > CharacterString",
+                formatter: "first"
+            },
+            isoii: {
+                selectors: "gmd\\:identificationInfo > gmd\\:MD_DataIdentification > gmd\\:resourceConstraints > gmd\\:MD_LegalConstraints > gmd\\:useLimitation > gco\\:CharacterString, identificationInfo > MD_DataIdentification > resourceConstraints > MD_LegalConstraints > useLimitation > CharacterString",
+                formatter: "first"
+            }
         }
     };
 
-    function _getPaths(fieldName) {
-        if (_.has(_paths, fieldName)) {
-            return _paths[fieldName];
+    function _getFieldConfigs(fieldName) {
+        if (_.has(_fieldConfigs, fieldName)) {
+            return _fieldConfigs[fieldName];
         }
         return null;
     }
 
-    function _getVal(path, xml) {
-        var val = null;
-        $(xml).find(path).each(function () {
-            // Expecting only one match xml element and act on the first.
-            val = $(this).text();
-            if (val) {
-                val = $.trim(val);
-            }
-            return; 
-        });
-        return val;
+    /**
+     * Returns formatted value of first matching selector
+     * 
+     * @param fieldConfig - object with selectors, one for each metadata standard.
+     * @param xml - parsed xml metadata document
+     */ 
+    function _getVal(fieldConfig, xml) {
+        var formatter = null;
+        //Set formatter
+        switch(fieldConfig.formatter) {
+            case 'first':
+                formatter = _getFirstVal
+                break;
+            case 'list':
+                formatter = _getListVal
+                break;
+            default:
+                formatter = _getFirstVal
+        }
+        var matchElements = $(xml).find(fieldConfig.selectors)        
+        return formatter(matchElements);
     }
 
+    //Return the text value of the first match
+    function _getFirstVal(elements) {        
+        var val = $(elements[0]).text();
+        if (val) {
+            return $.trim(val);
+        } else {
+            return null;
+        }
+    }
+    
+    //Return a combined list of all matches, separated by semicolon
+    function _getListVal(elements) {        
+        var val = elements.map(function() {
+            return $(this).text();
+        }).toArray().join("; ");
+        return val;
+    }
 
     return {
         
         /**
          * Finds and returns a value for the given metadata key. The key 
          * is used to get an xml path across multiple xml metadata formats 
-         * (ISO, DC, FGDC).
+         * (ISO, ISO2, DC, FGDC).
          * 
          * @param  {[type]} fieldName Key used to lookup value accross
          * multiple metadata formats.
@@ -31065,12 +31169,17 @@ angular.module('wcodpApp').factory('metadata', [function() {
          * the value as a string.
          */
         get: function (fieldName, xml) {
-            var paths = _getPaths(fieldName),
-                val = null;
-            _.some(paths, function(path, key) {
-                // Stops looping once a non-empty string is found.
-                val = _getVal(path, xml);
-                return val;
+            var fieldConfigs = _getFieldConfigs(fieldName);
+            //Global
+            val = null;
+            //For each field to display, get value from xml doc
+            _.some(fieldConfigs, function(fieldConfig, key) {
+                if (fieldConfig.selectors) {
+                    val = _getVal(fieldConfig, xml);
+                    return val;
+                } else {
+                    return null;
+                }
             });
             return val;
         },
