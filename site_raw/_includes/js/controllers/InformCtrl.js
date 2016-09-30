43ae19bdfcbc2b3e7a5def1a88ecce08fea2bbe8
Allison Bailey <allison@soundgis.com>
2016-09-30 12:27:04 -0700

    Remove draft SLR page changes

--- site_raw/_includes/js/controllers/InformCtrl.js
+++ site_raw/_includes/js/controllers/InformCtrl.js
@@ -3,6 +3,4 @@ angular.module('wcodpApp').controller('InformCtrl', ['$scope', function($scope) 
 	// Setup photo backgrounds.
 	$('.home-item-marine-debris')
 		.backstretch("/assets/img/photos/marinedebris_3.jpg");
-	$('.home-item-sea-level-rise')
-		.backstretch("/assets/img/photos/sea-level-rise.jpg");
 }]);
